55fbc1975e0dd8b4b9abb614583fd98e7cd5c946
Ju Liu <ju@noredink.com>
2020-02-27 17:05:19 +0100

    Some more ringtones

--- src/Main.elm
+++ src/Main.elm
@@ -71,19 +71,35 @@ theSimpsons =
     }
 
 
+harryPotter : Ringtone
+harryPotter =
+    { name = "Harry Potter"
+    , value = "8b1 8.e2 16g2 8#f2 4e2 8b2 4.a2 4.#f2 8.e2 16g2 8#f2 4d2 8f2 2b1 8- 8b1 8.e2 16g2 8#f2 4e2 8b2 4d3 8#c3 4c3 8#g2 8.c3 16b2 8#a2 4#f2 8g2 2e2 8- 8g2 4b2 8g2 4b2 8g2 4c3 8b2 4#a2 8#f2 8.g2 16b2 8#a2 4#a1"
+    , tempo = 125
+    }
+
+
+indianaJones : Ringtone
+indianaJones =
+    { name = "Indiana Jones"
+    , value = "4e2 8- 8f2 8g2 8- 1c3 8- 4d2 8- 8e2 1f2 4- 4g2 8- 8a2 8b2 8- 1f3 4- 4a2 8- 8b2 2c3 2d3 2e3 4e2 8- 8f2 8g2 8- 1c3 4- 4d3 8- 8e3 1f3 4-"
+    , tempo = 225
+    }
+
+
+missionImpossible : Ringtone
+missionImpossible =
+    { name = "Mission Impossible"
+    , value = "16g2 8- 16g2 8- 16f2 16- 16#f2 16- 16g2 8- 16g2 8- 16#a2 16- 16c3 16- 16g2 8- 16g2 8- 16f2 16- 16#f2 16- 16g2 8- 16g2 8- 16#a2 16- 16c3 16- 16#a2 16g2 2d2 32- 16#a2 16g2 2#c2 32- 16#a2 16g2 2c2 16- 16#a1 16c2 8-"
+    , tempo = 100
+    }
+
+
 myHeart : Ringtone
 myHeart =
     { name = "My Heart Will Go On"
     , value = "2d2 2e2 4a1 2a2 4g2 8#f2 2e2 4#f2 4g2 2#f2 4e2 4d2 4#c2 2d2 4#c2 1b1 1a1 1d2 2e2 4a1 2a2 4g2 8#f2 2e2 4#f2 4g2 2#f2 4e2 4d2 4#c2 2d2 4#c2 4#c2 2d2 4e2 2#f2 2e2 1d2"
     , tempo = 140
-    }
-
-
-neverGonna : Ringtone
-neverGonna =
-    { name = "Big Surprise"
-    , value = "8g1 8a1 8c2 8a1 4e2 8- 4e2 8- 4.d2 4- 8- 8g1 8a1 8c2 8a1 4d2 8- 4d2 8- 4c2 8b1 4.a1 8g1 8a1 8c2 8a1 2c2 4d2 4b1 8a1 4.g1 8- 4g1 2d2 2.c2 4-"
-    , tempo = 200
     }
 
 
@@ -95,11 +111,22 @@ classic =
     }
 
 
+neverGonna : Ringtone
+neverGonna =
+    { name = "Big Surprise"
+    , value = "8g1 8a1 8c2 8a1 4e2 8- 4e2 8- 4.d2 4- 8- 8g1 8a1 8c2 8a1 4d2 8- 4d2 8- 4c2 8b1 4.a1 8g1 8a1 8c2 8a1 2c2 4d2 4b1 8a1 4.g1 8- 4g1 2d2 2.c2 4-"
+    , tempo = 200
+    }
+
+
 allRingtones : List Ringtone
 allRingtones =
     [ theGoodTheBad
     , barbieGirl
     , theSimpsons
+    , harryPotter
+    , indianaJones
+    , missionImpossible
     , myHeart
     , classic
     , neverGonna
